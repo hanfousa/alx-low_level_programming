5165c7cd63883cd9f3d9a8a51deec49d6a106d31
hanfousa <125551224+hanfousa@users.noreply.github.com>
2023-09-28 22:24:33 +0100

    first commit

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * get_endianness - checks if a machine is little or big endian
+ * get_endianness - checking if a machine is little or big endian
  * Return: 0 for big, 1 for little
  */
 int get_endianness(void)
 {
-	unsigned int i = 1;
-	char *c = (char *) &i;
+	unsigned int l = 1;
+	char *c = (char *) &l;
 
 	return (*c);
 }
