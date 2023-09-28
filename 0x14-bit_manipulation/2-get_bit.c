6e095e60b63c35a1e5a1ce52d94e7208ccd68fc7
hanfousa <125551224+hanfousa@users.noreply.github.com>
2023-09-28 22:19:19 +0100

    first commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at an index in a decimal number
- * @n: number to search
- * @index: index of the bit
+ * get_bit - return the value of a bit at an index in a decimal number
+ * @n: number to be searched
+ * @index: the index of the bit
  *
- * Return: value of the bit
+ * Return: the value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
