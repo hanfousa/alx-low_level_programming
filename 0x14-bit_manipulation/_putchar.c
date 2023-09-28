fd5cfb10ac0e84b4c9deb5cf74c2ae80f8ece6de
hanfousa <125551224+hanfousa@users.noreply.github.com>
2023-09-28 22:10:27 +0100

    first commit

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @c: The character to be printed
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and errno is to be set appropriately.
  */
 int _putchar(char c)
 {
